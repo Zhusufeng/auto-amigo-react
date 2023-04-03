e18785b2ebd6f09ce2356998f8d9a78f5b170f17
Lisa Gee <zhusufeng@gmail.com>
2023-04-03 08:44:34 -0700

    Use mysql2

--- db/config.ts
+++ db/config.ts
@@ -1,4 +1,4 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
 export const connection = mysql.createConnection({
   host: "localhost",
@@ -7,6 +7,7 @@ export const connection = mysql.createConnection({
   database: "auto-amigo-react",
 });
 
+// TODO Is this needed now with mysql2?
 export const queryDb = (query: string) => {
   return new Promise((resolve, reject) => {
     connection.query(query, function (error, results) {
