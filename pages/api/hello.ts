e8cb571263da28d54badfe6d1c75b97bf91e5ee6
Lisa Gee <zhusufeng@gmail.com>
2023-04-03 08:45:25 -0700

    Test that database connection is working in /hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,16 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { connection } from "../../db/config";
+import { queryDb } from "../../db/config";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const conn = connection;
-  console.log("conn", conn);
+  const results = await queryDb("SELECT * FROM USER");
+  console.log(results);
   res.status(200).json({ name: "John Doe" });
 }
