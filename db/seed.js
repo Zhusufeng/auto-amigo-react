47f55e5c5fce977a50de4ba4085ae177553c9e3d
Lisa Gee <zhusufeng@gmail.com>
2023-04-03 12:11:06 -0700

    Create a seed file

---
+++ db/seed.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  // TODO: mysql2 is unable to get password
+  // This requires a fix
+  // In the meantime, replace variable with actual string
+  password: process.env.MYSQL_PASSWORD,
+  database: "auto-amigo-react",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const promisePool = pool.promise();
+
+async function seed() {
+  const seedUSERQuery = `
+    INSERT INTO USER (email, password)
+    VALUES ("me@me.com", "test");
+  `;
+  const seedGAS_LOGQuery = `
+    INSERT INTO GAS_LOG (userId, previousMileage, currentMileage, gallons, pricePerGallon)
+    VALUES (1, 0, 1000, 10.45, 5);
+  `;
+  await promisePool.query(seedUSERQuery);
+  await promisePool.query(seedGAS_LOGQuery);
+  console.log("Database was seeded");
+  return;
+}
+
+seed();
