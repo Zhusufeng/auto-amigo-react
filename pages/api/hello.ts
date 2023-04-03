5702468d41f2546962d00ac162697b66cf034949
Lisa Gee <zhusufeng@gmail.com>
2023-04-03 07:23:44 -0700

    Practice connecting to the db

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connection } from "../../db/config";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const conn = connection;
+  console.log("conn", conn);
+  res.status(200).json({ name: "John Doe" });
 }
