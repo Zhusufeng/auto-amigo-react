b51cc404c941c76d6c5f6f670088cb86479f06c5
Lisa Gee <zhusufeng@gmail.com>
2023-04-03 21:53:51 -0700

    Get data for GasTable

--- pages/components/GasTable.tsx
+++ pages/components/GasTable.tsx
@@ -1,3 +1,18 @@
+import { useState, useEffect } from "react";
+
 export default function GasTable() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch("/api/gas");
+    const data = await response.json();
+    console.log(data);
+    setData(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <div>GAS TABLE</div>;
 }
