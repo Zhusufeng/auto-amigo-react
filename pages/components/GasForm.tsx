604e3142c57764894ecde4c8ba353e9498be91f2
Lisa Gee <zhusufeng@gmail.com>
2023-04-04 07:46:31 -0700

    Build GasForm main structure

--- pages/components/GasForm.tsx
+++ pages/components/GasForm.tsx
@@ -1,3 +1,27 @@
 export default function GasForm() {
-  return <div>GAS FORM</div>;
+  return (
+    <form>
+      <div>
+        <div>
+          <label htmlFor="previousMileage">Previous Mileage</label>
+          <input type="text" name="previousMileage" required />
+        </div>
+        <div>
+          <label htmlFor="currentMileage">Current Mileage</label>
+          <input type="text" name="currentMileage" required />
+        </div>
+        <div>
+          <label htmlFor="gallons">Gallons</label>
+          <input type="text" name="gallons" required />
+        </div>
+        <div>
+          <label htmlFor="pricePerGallon">Price Per Gallon</label>
+          <input type="text" name="pricePerGallon" required />
+        </div>
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
+      </div>
+    </form>
+  );
 }
