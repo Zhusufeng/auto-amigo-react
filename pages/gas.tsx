8384b87fb82d58af9c37e3cf3eb458b4ed991ca0
Lisa Gee <zhusufeng@gmail.com>
2023-04-05 23:21:44 -0700

    Protect Gas Log page

---
+++ pages/gas.tsx
@@ -0,0 +1,26 @@
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import GasContainer from "./components/GasContainer";
+import NavBar from "./components/NavBar";
+
+export default function Home() {
+  const { data: session } = useSession();
+  return (
+    <>
+      <Head>
+        <title>Auto Amigo - Gas</title>
+        <meta name="description" content="Auto Amigo" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <NavBar />
+        {session ? (
+          <GasContainer />
+        ) : (
+          <div>You need to sign in to see your gas log</div>
+        )}
+      </main>
+    </>
+  );
+}
