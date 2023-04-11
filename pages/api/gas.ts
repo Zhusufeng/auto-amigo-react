4a876425e4abf29425a50a2638e785b799a4835a
Lisa Gee <zhusufeng@gmail.com>
2023-04-11 07:26:00 -0700

    Add try/catch & use req.query in getGas API

--- pages/api/gas.ts
+++ pages/api/gas.ts
@@ -1,62 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "./auth/[...nextauth]";
 import { promisePool } from "../../db/config";
 
 type Data = {
   name: string;
 };
 
-const getUserId = async (
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-): Promise<number> => {
+const getGas = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
-    console.log("I am in getUserId");
-    const session = await getServerSession(req, res, authOptions);
-    console.log("session", session);
-    if (session?.user?.email) {
-      const query = `
-        SELECT id FROM USER WHERE email = "${session.user.email}";
-      `;
-      const [results] = await promisePool.query(query);
-      console.log(results);
-      // If empty, add user to USER table and return id
-      // If not empty, return the id!
-      return 1;
-    } else {
-      return 0;
+    const { userId } = req.query;
+    if (!userId) {
+      throw new Error(
+        "Incomplete call. This API requires the userId as a parameter."
+      );
     }
-    return 0;
-  } catch (error) {
-    console.log(error);
-    return 0;
+    const query = `
+      SELECT * FROM GAS_LOG WHERE userId = ${userId};
+    `;
+    const [results] = await promisePool.query(query);
+    return res.status(200).json(results);
+  } catch (error: any) {
+    return res.status(400).json(error);
   }
 };
 
-const getGas = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  // TODO When we have multiple users, update to take in userId!
-  // TODO add try/catch
-  const id = await getUserId(req, res);
-  const userId = 1;
-  const query = `
-    SELECT * FROM GAS_LOG WHERE userId = ${userId};
-  `;
-  const [results] = await promisePool.query(query);
-  return res.status(200).json(results);
-};
-
 const postGas = async (req: NextApiRequest, res: NextApiResponse<any>) => {
-  // TODO add try/catch
-  const { userId, previousMileage, currentMileage, gallons, pricePerGallon } =
-    req.body;
-  const query = `
-    INSERT INTO GAS_LOG (userId, previousMileage, currentMileage, gallons, pricePerGallon)
-    VALUES (${userId}, ${previousMileage}, ${currentMileage}, ${gallons}, ${pricePerGallon});
-  `;
-  const [results] = await promisePool.query(query);
-  return res.status(200).json(results);
+  try {
+    const { userId, previousMileage, currentMileage, gallons, pricePerGallon } =
+      req.body;
+    const query = `
+      INSERT INTO GAS_LOG (userId, previousMileage, currentMileage, gallons, pricePerGallon)
+      VALUES (${userId}, ${previousMileage}, ${currentMileage}, ${gallons}, ${pricePerGallon});
+    `;
+    const [results] = await promisePool.query(query);
+    return res.status(200).json(results);
+  } catch (error: any) {
+    return res.status(400).json(error);
+  }
 };
 
 export default async function handler(
