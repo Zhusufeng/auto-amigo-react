5a98f5f20983a73e927d26295c9527a2549811ab
Lisa Gee <zhusufeng@gmail.com>
2023-04-11 07:04:01 -0700

    Call getUserIdByEmail and createUser in nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { getUserIdByEmail, createUser } from "../user";
 
 export const authOptions = {
   providers: [
@@ -8,6 +9,35 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    session: async (session, user) => {
+      try {
+        console.log("session", session);
+        if (session?.session?.user?.email) {
+          const email = session?.session?.user?.email;
+          let userId;
+
+          const findUserResult = await getUserIdByEmail(email);
+          console.log("nextauth findUserResult", findUserResult);
+
+          // If no user is found in the db, create one
+          if (findUserResult === 0) {
+            const createUserResult = await createUser(email);
+            userId = createUserResult;
+          } else {
+            userId = findUserResult;
+          }
+          session.session.user = {
+            ...session.session.user,
+            userId: result,
+          };
+        }
+        return Promise.resolve(session);
+      } catch (error) {
+        console.log("callbacks error", error);
+      }
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
