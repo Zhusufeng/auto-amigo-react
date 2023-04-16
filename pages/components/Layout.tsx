6af9190530114fd7830af6bfab2def5ab1b95dd4
Lisa Gee <zhusufeng@gmail.com>
2023-04-16 08:29:39 -0700

    Create Layout component

---
+++ pages/components/Layout.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Layout } from "antd";
+import NavBar from "./NavBar";
+const { Header, Footer, Content } = Layout;
+
+const layoutStyle: React.CSSProperties = {
+  height: "100vh",
+  width: "100%",
+};
+
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 50,
+  lineHeight: "64px",
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: "center",
+  height: 64,
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MainLayout = ({ children }: Props) => {
+  return (
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>
+        <NavBar />
+      </Header>
+      <Content style={contentStyle}>{children}</Content>
+      <Footer style={footerStyle}>Insert Footer Here</Footer>
+    </Layout>
+  );
+};
+
+export default MainLayout;
