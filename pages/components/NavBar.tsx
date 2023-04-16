7d37a0703da724eb6a8162111d9c6fa362a45fa9
Lisa Gee <zhusufeng@gmail.com>
2023-04-16 08:58:12 -0700

    Use an object for style in NavBar

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,25 +1,29 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
+const itemStyle: React.CSSProperties = {
+  padding: "0 10px",
+};
+
 const NavBar = () => {
   const { data: session } = useSession();
 
   return (
     <div style={{ display: "flex" }}>
-      <div style={{ padding: "0 10px" }}>
+      <div style={itemStyle}>
         <Link href="/">Auto Amigo</Link>
       </div>
-      <div style={{ padding: "0 10px" }}>
+      <div style={itemStyle}>
         <Link href="/gas">Gas Log</Link>
       </div>
-      <div style={{ padding: "0 10px" }}>
+      <div style={itemStyle}>
         {session ? (
           <>Signed in as {session?.user?.email}</>
         ) : (
           <>Not signed in</>
         )}
       </div>
-      <div style={{ padding: "0 10px" }}>
+      <div style={itemStyle}>
         {session ? (
           <button onClick={() => signOut()}>Sign out</button>
         ) : (
