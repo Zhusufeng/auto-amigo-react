327da6a6a8fe20ad96c7768f7ac05b72fff5c08d
Lisa Gee <zhusufeng@gmail.com>
2023-04-16 08:34:58 -0700

    Use Head component in gas and index pages

--- pages/gas.tsx
+++ pages/gas.tsx
@@ -1,19 +1,13 @@
 import { useSession } from "next-auth/react";
-import Head from "next/head";
+import Head from "./components/Head";
 import Layout from "./components/Layout";
 import GasContainer from "./components/GasContainer";
-import NavBar from "./components/NavBar";
 
 export default function Home() {
   const { data: session } = useSession();
   return (
     <>
-      <Head>
-        <title>Auto Amigo - Gas</title>
-        <meta name="description" content="Auto Amigo" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head title="Auto Amgio - Gas" />
       <Layout>
         {session ? (
           <GasContainer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
-import Head from "next/head";
+import Head from "./components/Head";
 import Layout from "./components/Layout";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Auto Amigo</title>
-        <meta name="description" content="Auto Amigo" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head title="Auto Amigo - Home" />
       <Layout>Welcome to Auto Amigo!</Layout>
     </>
   );
