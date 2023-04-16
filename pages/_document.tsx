1b353467124839c9b87af850ff73a846271348c0
Lisa Gee <zhusufeng@gmail.com>
2023-04-16 08:28:11 -0700

    Give body no margin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+
+const bodyStyle: React.CSSProperties = {
+  margin: 0,
+};
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={bodyStyle}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
