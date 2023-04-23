a9be8b2ff69f4e9121be68aee673f22c57d7e783
Lisa Gee <zhusufeng@gmail.com>
2023-04-23 09:40:05 -0700

    Add some instructions to GasForm

--- pages/components/GasForm.tsx
+++ pages/components/GasForm.tsx
@@ -39,52 +39,55 @@ export default function GasForm(props: Props) {
   };
 
   return (
-    <Form
-      name="basic"
-      onFinish={onSubmit}
-      form={form}
-      {...formItemLayout}
-      style={{ maxWidth: 600 }}
-    >
-      <Form.Item
-        label="Previous Mileage"
-        name="previousMileage"
-        rules={[
-          { required: true, message: "Please input your previous mileage!" },
-        ]}
+    <div>
+      <div>Keep track of your gas history!</div>
+      <Form
+        name="basic"
+        onFinish={onSubmit}
+        form={form}
+        {...formItemLayout}
+        style={{ maxWidth: 600 }}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Current Mileage"
-        name="currentMileage"
-        rules={[
-          { required: true, message: "Please input your current mileage!" },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Gallons"
-        name="gallons"
-        rules={[{ required: true, message: "Please input your gallons!" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Price Per Gallon"
-        name="pricePerGallon"
-        rules={[
-          { required: true, message: "Please input the price per gallon!" },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Previous Mileage"
+          name="previousMileage"
+          rules={[
+            { required: true, message: "Please input your previous mileage!" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Current Mileage"
+          name="currentMileage"
+          rules={[
+            { required: true, message: "Please input your current mileage!" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Gallons"
+          name="gallons"
+          rules={[{ required: true, message: "Please input your gallons!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Price Per Gallon"
+          name="pricePerGallon"
+          rules={[
+            { required: true, message: "Please input the price per gallon!" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
