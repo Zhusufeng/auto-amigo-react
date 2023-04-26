267b934b7a293081d6f6a31360a8850c3de5c324
Lisa Gee <zhusufeng@gmail.com>
2023-04-26 11:29:42 -0700

    Add DELETE and PATCH to /gas

--- pages/api/gas.ts
+++ pages/api/gas.ts
@@ -6,6 +6,20 @@ type Data = {
   name: string;
 };
 
+const deleteGas = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  try {
+    const { gasId } = req.body;
+    const query = `
+      DELETE FROM GAS_LOG
+      WHERE id = ${gasId}
+    `;
+    const [results] = await promisePool.query(query);
+    return res.status(204).json(results);
+  } catch (error: any) {
+    return res.status(400).json(error);
+  }
+};
+
 const getGas = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     const { userId } = req.query;
@@ -24,6 +38,26 @@ const getGas = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   }
 };
 
+const patchGas = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  try {
+    const { gasId, previousMileage, currentMileage, gallons, pricePerGallon } =
+      req.body;
+    const query = `
+      UPDATE GAS_LOG
+      SET 
+        previousMileage = ${previousMileage}
+        currentMileage = ${currentMileage}
+        gallons = ${gallons}
+        pricePerGallon = ${pricePerGallon}
+      WHERE id = ${gasId}
+    `;
+    const [results] = await promisePool.query(query);
+    return res.status(200).json(results);
+  } catch (error: any) {
+    return res.status(400).json(error);
+  }
+};
+
 const postGas = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   try {
     const { userId, previousMileage, currentMileage, gallons, pricePerGallon } =
@@ -33,7 +67,7 @@ const postGas = async (req: NextApiRequest, res: NextApiResponse<any>) => {
       VALUES (${userId}, ${previousMileage}, ${currentMileage}, ${gallons}, ${pricePerGallon});
     `;
     const [results] = await promisePool.query(query);
-    return res.status(200).json(results);
+    return res.status(201).json(results);
   } catch (error: any) {
     return res.status(400).json(error);
   }
@@ -45,14 +79,20 @@ export default async function handler(
 ) {
   const { method } = req;
   switch (method) {
+    case "DELETE":
+      await deleteGas(req, res);
+      break;
     case "GET":
       await getGas(req, res);
+      break;
+    case "PATCH":
+      await patchGas(req, res);
       break;
     case "POST":
       await postGas(req, res);
       break;
     default:
-      res.setHeader("Allow", ["GET", "POST"]);
+      res.setHeader("Allow", ["DELETE", "GET", "PATCH", "POST"]);
       res.status(405).end(`Method ${method} Not Allowed`);
       break;
   }
