8035e44dcc6ede84f34aa5be272342efe245a71a
Lisa Gee <zhusufeng@gmail.com>
2023-04-26 12:19:30 -0700

    Add onClicks to Edit and Delete

--- pages/components/GasTable.tsx
+++ pages/components/GasTable.tsx
@@ -1,47 +1,4 @@
-import { Table } from "antd";
-
-const columns = [
-  {
-    title: "Date",
-    dataIndex: "createdAt",
-    key: "createdAt",
-  },
-  {
-    title: "Previous Mileage",
-    dataIndex: "previousMileage",
-    key: "previousMileage",
-  },
-  {
-    title: "Current Mileage",
-    dataIndex: "currentMileage",
-    key: "currentMileage",
-  },
-  { title: "Miles Driven", dataIndex: "milesDriven", key: "milesDriven" },
-  { title: "Gallons", dataIndex: "gallons", key: "gallons" },
-  { title: "MPG", dataIndex: "mpg", key: "mpg" },
-  {
-    title: "Price Per Gallon",
-    dataIndex: "pricePerGallon",
-    key: "pricePerGallon",
-  },
-  { title: "Total Spent", dataIndex: "totalSpent", key: "totalSpent" },
-  {
-    title: "Edit",
-    render: () => {
-      return <a>Edit</a>;
-    },
-  },
-  {
-    title: "Delete",
-    render: () => {
-      return <a>Delete</a>;
-    },
-  },
-];
-
-type Props = {
-  data: GasEntry[];
-};
+import { Table, Button } from "antd";
 
 type GasEntry = {
   id: number;
@@ -54,8 +11,14 @@ type GasEntry = {
   createdAt: string;
 };
 
+type Props = {
+  data: GasEntry[];
+  handleEdit: (record: GasEntry) => void;
+  handleDelete: (record: GasEntry) => void;
+};
+
 const GasTable = (props: Props) => {
-  const { data } = props;
+  const { data, handleEdit, handleDelete } = props;
 
   const tableData = data.map(entry => {
     const { id, previousMileage, currentMileage, gallons, pricePerGallon } =
@@ -66,6 +29,46 @@ const GasTable = (props: Props) => {
     const totalSpent = (gallons * pricePerGallon).toFixed(2);
     return { ...entry, key, milesDriven, mpg, totalSpent };
   });
+
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "createdAt",
+      key: "createdAt",
+    },
+    {
+      title: "Previous Mileage",
+      dataIndex: "previousMileage",
+      key: "previousMileage",
+    },
+    {
+      title: "Current Mileage",
+      dataIndex: "currentMileage",
+      key: "currentMileage",
+    },
+    { title: "Miles Driven", dataIndex: "milesDriven", key: "milesDriven" },
+    { title: "Gallons", dataIndex: "gallons", key: "gallons" },
+    { title: "MPG", dataIndex: "mpg", key: "mpg" },
+    {
+      title: "Price Per Gallon",
+      dataIndex: "pricePerGallon",
+      key: "pricePerGallon",
+    },
+    { title: "Total Spent", dataIndex: "totalSpent", key: "totalSpent" },
+    {
+      title: "Edit",
+      render: (_: any, record: GasEntry) => {
+        return <Button onClick={() => handleEdit(record)}>Edit</Button>;
+      },
+    },
+    {
+      title: "Delete",
+      render: (_: any, record: GasEntry) => {
+        // TODO Add button and a confirmation
+        return <Button onClick={() => handleDelete(record)}>Delete</Button>;
+      },
+    },
+  ];
 
   return (
     <div>
