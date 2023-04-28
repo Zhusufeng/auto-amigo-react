b8bca7b553995d4e6d44dc320bde78853c7bfef2
Lisa Gee <zhusufeng@gmail.com>
2023-04-28 08:28:39 -0700

    Add React Query to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
